7102d479f9dd29b08c50e1caad159827649f3c97
mihai <mihail_danilchuk@mail.ru>
2022-02-16 00:29:17 +0300

    added axes and fixed alg

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from math import fabs as abs
 from PIL import Image, ImageDraw
 
 
+WIDTH = 81
+HEIGHT = 81
+
+
 def math_round(number):
     if number - int(number) > 0.5:
         return int(number + 1)
@@ -17,27 +21,39 @@ def sign(number):
         return 0
 
 
+def to_normal_view(x, y):
+    return x + WIDTH // 2, -1 * (y - HEIGHT // 2)
+
+
+def wright_axes():
+    dda(-1 * WIDTH // 2, 0, WIDTH // 2 + 1, 0, 'red')
+    dda(0, -1 * HEIGHT // 2, 0, HEIGHT // 2 + 1, 'red')
+
+
 x1, y1, x2, y2 = [int(i) for i in input("Input: x1, y1, x2, y2: ").split()]
-img = Image.new('RGB', (20, 20), 'white')
+img = Image.new('RGB', (HEIGHT, WIDTH), 'white')
 idDraw = ImageDraw.Draw(img)
 
 
-def dda(x1, y1, x2, y2):
+def dda(x1, y1, x2, y2, color = 'black'):
     if abs(x2-x1) >= abs(y2-y1):
         length = abs(x2-x1)
     else:
         length = abs(y2-y1)
     length = int(length)
-    dx = (x2 - x1) // length
-    dy = (y2 - y1) // length
+    dx = (x2 - x1) / length
+    dy = (y2 - y1) / length
     x = x1 + 0.5 * sign(dx)
     y = y1 + 0.5 * sign(dy)
     for i in range(0, length):
-        idDraw.point((math_round(x), math_round(y)), fill='black')
-        print(math_round(x), math_round(y))
+        idDraw.point(to_normal_view(math_round(x), math_round(y)), fill=color)
+        #print(math_round(x), math_round(y))
+        print(x, y)
         x += dx
         y += dy
 
 
+wright_axes()
 dda(x1, y1, x2, y2)
+
 img.save("ans.png")
